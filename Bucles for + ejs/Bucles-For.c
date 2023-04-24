b48787f422bca4f2db0a2e2d361a083aaef60aa3
maximo llompart <maximollompart@gmail.com>
2023-04-24 11:20:10 -0300

    agreguewhile y dowhile

---
+++ Bucles for + ejs/Bucles-For.c
@@ -0,0 +1,29 @@
+// BUCLES FOR  
+//los bucles for sirven para cuando sabemos exactamente la cantidad de veces que queremos
+//que se ejecute el codigo
+//siempre requiere de una variable entera que cumpla la funcion de contador
+
+//ej:
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(){
+    //int i = 1123;
+    
+    for (int i = 1; i <= 100; i++) //aca siempre ; nunca , 
+    {
+        printf("%d-", i);
+    }
+    //este es un contador del 1 al 100
+    return 0;
+}
+
+//Este es un ej de bucle infinito porque la condicion nunca va a ser falsa:
+int main() {
+
+    for (int i = 1; i < 100; i--) //esto es debido a que i siempre va a ser menor a 100 
+    {
+        printf("%d - ", i);
+    }
+    return 0;
+}
---
+++ Bucles for + ejs/ej_buclefor.c
@@ -0,0 +1,16 @@
+//Este ej calcula e informa la suma de 10 num reales ingresados por teclado
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+    float numeroIngresado, suma = 0;
+    for(int i = 1; i <= 10; i++);
+    {
+        printf("ingresa un numero %d: \n", i);
+        scanf("&f", &numeroIngresado);
+        suma += numeroIngresado;
+    }
+    printf("La suma es = %.2f \n", suma);
+
+    return 0;
+}
+//no me anda no se porque
